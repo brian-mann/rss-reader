54571dc3981ab86a6a7b5e08a46784bbb6e26605
Brian Mann <brian@osysit.com>
2013-05-08 00:37:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReader::Application.config.secret_token = 'ddd94ec4a5a0c5b61ed1a1a2ac58907d205dab99ebeeb732e9f5f5177dd140a5e7672fad1b9b7226fde8b56e52116daf8d0aff2e02cebb1e50eb2209584607e7'
